0e9bc26138f97fdf0248343d213729a9413158f5
SoilZhu <soilzhu80@gmail.com>
2026-09-23 17:12:46 +0800

    feat: 更新应用版本至1.0.7

--- lib/features/profile/screens/about_screen.dart
+++ lib/features/profile/screens/about_screen.dart
@@ -72,7 +72,7 @@ class AboutScreen extends StatelessWidget {
               child: FutureBuilder<PackageInfo>(
                 future: PackageInfo.fromPlatform(),
                 builder: (context, snapshot) {
-                  final versionStr = snapshot.hasData ? snapshot.data!.version : '1.0.6';
+                  final versionStr = snapshot.hasData ? snapshot.data!.version : '1.0.7';
                   return Text(
                     'Version $versionStr',
                     style: TextStyle(
